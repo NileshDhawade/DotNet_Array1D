a4b5765d47c4d25a78b9961d3a4bfebd88280280
Nilesh Dhawade <dhawade.nilesh007@gmail.com>
2022-03-15 16:49:09 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tsystem_Array_1d
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
